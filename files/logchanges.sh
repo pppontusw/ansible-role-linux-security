942d80f548882b057cb4358c88e909901ffe90e8
Pontus Wiberg <pontus.wiberg@midaxo.com>
2017-04-04 16:01:54 +0300

    Proposed log permission changes (not used by role currently)

---
+++ files/logchanges.sh
@@ -0,0 +1,15 @@
+cd /var/log
+sudo chmod 640 aptitude 
+sudo chmod 640 awslogs*
+sudo chmod 640 cloud-init*
+sudo chmod 640 alternatives.log 
+sudo chmod 640 lastlog 
+sudo chmod 640 logstatus 
+sudo chmod 640 wtmp
+sudo chmod 750 dist-upgrade/
+sudo chmod 750 fsck/
+sudo chmod 750 nginx/
+sudo chmod 750 ntpstats/
+sudo chmod 750 lxd/
+sudo chmod 750 apt
+sudo chmod 640 dpkg.log 
