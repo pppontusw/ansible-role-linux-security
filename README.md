ec70a6cf662543276bb6635d51f71c25c40a13cd
Pontus Wiberg <pontus.wiberg@midaxo.com>
2017-04-07 15:23:22 +0300

    SO-155 Disk partitioning and migration script

--- README.md
+++ README.md
@@ -25,90 +25,9 @@ IF PARTITIONING IS SET TO ON YOU NEED TO FIRST CREATE THE PARTITIONS:
 
 2. Log in to the server and switch to root ```sudo su root```
 
-3. Create the below partitions using ```cfdisk /dev/xvda```
+3. Copy, chmod +x and run the the script partition.sh which will partition the disk and migrate /tmp /var /var/log /var/log/audit and /home to new partitions
 
-```
-/dev/xvda2 3G primary
-/dev/xvda3 5G primary
-/dev/xvda4 12G extended
-/dev/xvda5 5G primary
-/dev/xvda6 2G primary
-/dev/xvda7 5G primary
-```
-
-4. Run ```partprobe /dev/xvda``` to reload the kernel partition table
-
-5. Run the partition.sh script and then reboot
-
-```
-if grep "xvda2" /etc/fstab; then
-	echo "xvda2 is already specified in /etc/fstab, please double check what is going on"
-	exit 1
-fi
-
-# create the filesystems
-for i in {2,3,5,6,7}; 
-	do mkfs -t ext4 /dev/xvda$i; 
-done
-
-# create temporary mount points
-for i in {2,3,5,6,7}; 
-	do mkdir /mnt/xvda$i; 
-done
-
-# mount the new partitions in their temporary mount point
-for i in {2,3,5,6,7}; 
-	do mount /dev/xvda$i /mnt/xvda$i; 
-done
-
-# copy all the data to the new partitions
-cp -apx /tmp/* /mnt/xvda2
-cp -apx /var/* /mnt/xvda3
-cp -apx /var/log/* /mnt/xvda5
-if [-d /var/log/audit]; then
-	cp -apx /var/log/audit/* /mnt/xvda6
-	rm -rf /mnt/xvda5/audit
-else
-	touch /mnt/xvda6/nothing
-fi
-cp -apx /home/* /mnt/xvda7
-rm -rf /mnt/xvda3/log
-
-# move all the in-use files to /bak for safekeeping
-mkdir /bak
-for i in /tmp /var /home; 
-	do mv $i /bak; 
-done
-
-# unmount the new partitions from their temporary mount points
-for i in {2,3,5,6,7}; 
-	do umount /dev/xvda$i; 
-done
-
-# create final mount points for the new partitions
-for i in /tmp /var /home; 
-	do mkdir $i; 
-done
-
-# mount all the new partitions in the correct target location
-mount /dev/xvda2 /tmp
-mount /dev/xvda3 /var
-mkdir /var/log
-mount /dev/xvda5 /var/log
-mkdir /var/log/audit
-mount /dev/xvda6 /var/log/audit
-mount /dev/xvda7 /home
-
-# put the configuration in fstab for persistence
-echo "/dev/xvda2      /tmp    ext4    rw    0 0
-/dev/xvda3      /var    ext4    rw   0 0
-/dev/xvda5     /var/log    ext4    rw    0 0
-/dev/xvda6     /var/log/audit    ext4    rw    0 0
-/dev/xvda7     /home   ext4    rw    0 0" >> /etc/fstab
-
-echo "You will now need to reboot the server in order for everything to work with your new partition structure, when the server boots and everything works you can safely delete /bak"
-```
-
+4. Reboot
 
 
 Variables
--- partition.sh
+++ partition.sh
@@ -1,66 +1,128 @@
-if grep "xvda2" /etc/fstab; then
-	echo "xvda2 is already specified in /etc/fstab, please double check what is going on"
+#!/bin/bash
+if [ $(/sbin/parted -lm | grep ext4 | wc -l) -gt 1 ]
+then
+	echo "Looks like your disk is already partitioned, exiting.."
+	exit 1
+else
+	# to create the partitions programatically (rather than manually)
+	# we're going to simulate the manual input to fdisk
+	# The sed script strips off all the comments so that we can
+	# document what we're doing in-line with the actual commands
+	# Note that a blank line (commented as "defualt" will send a empty
+	# line terminated with a newline to take the fdisk default.
+	sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/xvda
+	  n # new partition to fill the first partition gap that's like 9MB
+	  p # primary partition
+	  2 # partition number 2
+	    # default - start at beginning of disk
+	    # fill size
+	  n # new partition
+	  p # primary partition
+	  3 # partition number 2
+	    # default - start immediately after root partition
+	  +3G # size
+	  n # new partition
+	  e # primary partition
+	  4 # partion number 3
+	    # default, start immediately after preceding partition
+	   	# fill size
+	  n # new partition
+	  5 # partion number 4
+	    # default, start immediately after preceding partition
+	  +5G # size
+	  n # new partition
+	  6 # partion number 5
+	    # default, start immediately after preceding partition
+	  +5G # size
+	  n # new partition
+	  7 # partion number 6
+	    # default, start immediately after preceding partition
+	  +2G # size
+	  n # new partition
+	  8 # partion number 7
+	    # default, start immediately after preceding partition
+	   # fill the rest of the disk
+	  p # print the in-memory partition table
+	  w # write the partition table
+	  q # and we're done
+EOF
+fi
+
+partprobe /dev/xvda
+
+if grep "xvda" /etc/fstab
+then
+	echo "xvda is already specified in /etc/fstab, please double check what is going on"
 	exit 1
 fi
 
 # create the filesystems
-for i in {2,3,5,6,7}; 
-	do mkfs -t ext4 /dev/xvda$i; 
+for i in {3,5,6,7,8}
+do
+	mkfs -t ext4 /dev/xvda$i
 done
 
 # create temporary mount points
-for i in {2,3,5,6,7}; 
-	do mkdir /mnt/xvda$i; 
+for i in {3,5,6,7,8}
+do
+	mkdir /mnt/xvda$i
 done
 
 # mount the new partitions in their temporary mount point
-for i in {2,3,5,6,7}; 
-	do mount /dev/xvda$i /mnt/xvda$i; 
+for i in {3,5,6,7,8}
+do
+	mount /dev/xvda$i /mnt/xvda$i
 done
 
 # copy all the data to the new partitions
-cp -apx /tmp/* /mnt/xvda2
-cp -apx /var/* /mnt/xvda3
-cp -apx /var/log/* /mnt/xvda5
-if [-d /var/log/audit]; then
-	cp -apx /var/log/audit/* /mnt/xvda6
-	rm -rf /mnt/xvda5/audit
+cp -apx /tmp/* /mnt/xvda3
+cp -apx /var/* /mnt/xvda5
+cp -apx /var/log/* /mnt/xvda6
+if [ -d "/var/log/audit" ]
+then
+	cp -apx /var/log/audit/* /mnt/xvda7
+	rm -rf /mnt/xvda6/audit
 else
-	touch /mnt/xvda6/nothing
+	touch /mnt/xvda7/nothing
 fi
-cp -apx /home/* /mnt/xvda7
-rm -rf /mnt/xvda3/log
+cp -apx /home/* /mnt/xvda8
+rm -rf /mnt/xvda5/log
 
 # move all the in-use files to /bak for safekeeping
 mkdir /bak
-for i in /tmp /var /home; 
-	do mv $i /bak; 
+for i in /tmp /var /home
+do
+	mv $i /bak
 done
 
 # unmount the new partitions from their temporary mount points
-for i in {2,3,5,6,7}; 
-	do umount /dev/xvda$i; 
+for i in {3,5,6,7,8}
+do
+	umount /dev/xvda$i
 done
 
 # create final mount points for the new partitions
-for i in /tmp /var /home; 
-	do mkdir $i; 
+for i in /tmp /var /home
+do
+	mkdir $i
 done
 
 # mount all the new partitions in the correct target location
-mount /dev/xvda2 /tmp
-mount /dev/xvda3 /var
+mount /dev/xvda3 /tmp
+mount /dev/xvda5 /var
 mkdir /var/log
-mount /dev/xvda5 /var/log
+mount /dev/xvda6 /var/log
 mkdir /var/log/audit
-mount /dev/xvda6 /var/log/audit
-mount /dev/xvda7 /home
+mount /dev/xvda7 /var/log/audit
+mount /dev/xvda8 /home
+
+rm /var/log/audit/nothing
 
 # put the configuration in fstab for persistence
-echo "/dev/xvda2      /tmp    ext4    rw    0 0
-/dev/xvda3      /var    ext4    rw   0 0
-/dev/xvda5     /var/log    ext4    rw    0 0
-/dev/xvda6     /var/log/audit    ext4    rw    0 0
-/dev/xvda7     /home   ext4    rw    0 0" >> /etc/fstab
+echo "/dev/xvda3      /tmp    ext4    rw    0 0
+/dev/xvda5      /var    ext4    rw   0 0
+/dev/xvda6     /var/log    ext4    rw    0 0
+/dev/xvda7     /var/log/audit    ext4    rw    0 0
+/dev/xvda8     /home   ext4    rw    0 0" >> /etc/fstab
 
 echo "You will now need to reboot the server in order for everything to work with your new partition structure, when the server boots and everything works you can safely delete /bak"
